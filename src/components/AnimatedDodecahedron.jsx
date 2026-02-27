e4382c7858d53e2f3767cfce1fafbcf7801a12a0
AlexMuia31 <alexmuia31@gmail.com>
2026-02-27 18:03:02 +0300

    added useSpring

--- src/components/AnimatedDodecahedron.jsx
+++ src/components/AnimatedDodecahedron.jsx
@@ -2,6 +2,59 @@ import { Dodecahedron } from "@react-three/drei";
 import { useSpring } from "@react-spring/three";
 
 export const AnimatedDodecahedron = () => {
+  const { x, y, rotationX, rotationZ } = useSpring({
+    from: {
+      x: -1,
+      y: 1,
+      rotationX: 0,
+      rotationZ: 0,
+    },
+    to: [
+      {
+        x: 1,
+        y: 1,
+        delay: 500,
+      },
+      {
+        x: -1,
+        y: 0,
+        rotationX: Math.PI,
+        rotationZ: Math.PI,
+        delay: 50,
+      },
+      {
+        x: 1,
+        y: 0,
+        delay: 50,
+      },
+      {
+        x: -1,
+        y: -1,
+        rotationX: Math.PI * 2,
+        rotationZ: Math.PI * 2,
+        delay: 50,
+      },
+      {
+        x: 1,
+        y: -1,
+        delay: 50,
+      },
+      {
+        x: -1,
+        y: 1,
+        rotationX: 0,
+        rotationZ: 0,
+        delay: 500,
+      },
+    ],
+    config: {
+      mass: 4,
+      tension: 600,
+      friction: 80,
+    },
+    loop: true,
+    immediate: true,
+  });
   return (
     <group position-x={-1} position-y={1}>
       <Dodecahedron>
